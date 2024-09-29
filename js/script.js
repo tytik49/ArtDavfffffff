5c291f37907cb33da4c93c7d672b4411c292bf8b
tytik49 <152650072+tytik49@users.noreply.github.com>
2024-09-29 21:15:36 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 document.getElementById("hamburger").onclick = function() {
-    document.querySelector(".overlay").style.display = "block";
+    const overlay = document.querySelector(".overlay");
+    overlay.style.display = "block";
+    setTimeout(() => overlay.style.opacity = "1", 10); // Задержка для плавного появления
 }
 
 document.querySelector(".overlay").onclick = function() {
-    this.style.display = "none";
+    this.style.opacity = "0";
+    setTimeout(() => this.style.display = "none", 300); // Задержка для плавного исчезновения
 }
