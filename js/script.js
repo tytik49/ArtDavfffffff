bcdf04c860cd9c048d22d6dae60fadd1ceee3c5b
tytik49 <152650072+tytik49@users.noreply.github.com>
2024-09-29 17:24:55 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
+// Открытие меню при нажатии на три полоски
 document.getElementById("hamburger").addEventListener("click", function() {
     document.getElementById("menuOverlay").style.display = "flex";
 });
 
-document.getElementById("menuOverlay").addEventListener("click", function() {
-    this.style.display = "none";
+// Закрытие меню при нажатии на область за его пределами
+document.getElementById("menuOverlay").addEventListener("click", function(event) {
+    if (event.target === this) { // Проверка, что нажата область вне меню
+        this.style.display = "none";
+    }
 });
