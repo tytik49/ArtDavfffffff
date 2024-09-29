f26d2ed00b7c3337f7938015c9c6e59b0d57b8b5
tytik49 <152650072+tytik49@users.noreply.github.com>
2024-09-29 21:52:54 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 document.getElementById("hamburger").onclick = function() {
     const overlay = document.querySelector(".overlay");
     overlay.style.display = "block";
-    setTimeout(() => overlay.style.opacity = "1", 10); // Задержка для плавного появления
 }
 
 document.querySelector(".overlay").onclick = function() {
-    this.style.opacity = "0";
-    setTimeout(() => this.style.display = "none", 300); // Задержка для плавного исчезновения
+    this.style.display = "none";
 }
