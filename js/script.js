89afd9da1109f2f830fc77324709a502dfaa238e
tytik49 <152650072+tytik49@users.noreply.github.com>
2024-09-29 13:04:48 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+// Получаем элементы
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav");
+
+// Добавляем обработчик события клика на гамбургер
+hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    if (nav.style.display === "none" || nav.style.display === "") {
+        nav.style.display = "block";
+    } else {
+        nav.style.display = "none";
+    }
+});
 
