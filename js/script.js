bbfdd3d575285757324295c63e92e77633e7ce3b
tytik49 <152650072+tytik49@users.noreply.github.com>
2024-09-29 13:56:19 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
-const hamburger = document.getElementById('hamburger');
-const nav = document.getElementById('nav');
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const nav = document.getElementById('nav');
 
-// Обработчик события для иконки гамбургера
-hamburger.addEventListener('click', () => {
-    nav.classList.toggle('active'); // Переключение класса 'active' для открытия/закрытия меню
-});
-
-// Закрытие меню при клике на ссылку
-const navLinks = document.querySelectorAll('.nav a');
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        nav.classList.remove('active'); // Убираем класс 'active' при клике на ссылку
+    hamburger.addEventListener('click', () => {
+        nav.classList.toggle('active'); // Открыть/закрыть меню
+        hamburger.classList.toggle('active'); // Для анимации гамбургера
     });
 });
