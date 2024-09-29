e3d167e872d51467923a780a2ef362d2fc6c37e0
tytik49 <152650072+tytik49@users.noreply.github.com>
2024-09-29 17:33:37 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-// Открытие меню при нажатии на три полоски
+// Открытие и закрытие меню при нажатии на три полоски
 document.getElementById("hamburger").addEventListener("click", function() {
-    document.getElementById("menuOverlay").style.display = "flex";
+    let overlay = document.getElementById("menuOverlay");
+    overlay.classList.toggle("active"); // Добавляем или убираем класс active
 });
 
-// Закрытие меню при нажатии на область за его пределами
+// Закрытие меню при клике на область за пределами меню
 document.getElementById("menuOverlay").addEventListener("click", function(event) {
-    if (event.target === this) { // Проверка, что нажата область вне меню
-        this.style.display = "none";
+    if (event.target === this) {
+        this.classList.remove("active"); // Убираем класс active для скрытия меню
     }
 });
